68dc3330eb651343d485c7fd106bb4bb270ac59f
Isabel Cruz <anaisabelcruz@Anas-MacBook-2.local>
2019-10-03 09:03:27 -0500

    core functionalities

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-$(document).ready(function main() {
+const main = () => {
   $('#addBtn').click(function() {
     event.preventDefault();
     let input = $('#inputText').val();
@@ -8,34 +8,35 @@ $(document).ready(function main() {
       let newItem =
         '<li>' +
         input +
-        '</li>' +
-        "<button id='checkBtn'>Check</button>" +
-        "<button id='deleteBtn'>Delete</button>";
+        "<button class='checkBtn'>Check</button>" +
+        "<button class='deleteBtn'>Delete</button>" +
+        '</li>';
       $('#shoppingList').append(newItem);
       $('#message').text(' ');
     }
   });
 
-  $('#shoppingList').on('click', 'button', function() {
-    event.preventDefault();
-    if ($(this).prop('id') === 'checkBtn') {
-      if (
-        $(this)
-          .parent()
-          .css('text-decoration') == 'line-through'
-      ) {
-        $(this)
-          .parent()
-          .css('text-decoration', 'none');
-      } else {
-        $(this)
-          .parent()
-          .css('text-decoration', 'line-through');
-      }
+  $('#shoppingList').on('click', '.checkBtn', function(e) {
+    e.preventDefault();
+    if (
+      $(this)
+        .parent()
+        .css('text-decoration') === 'line-through solid rgb(0, 0, 0)'
+    ) {
+      $(this)
+        .parent()
+        .css('text-decoration', 'none');
     } else {
       $(this)
         .parent()
-        .remove();
+        .css('text-decoration', 'line-through solid rgb(0, 0, 0)');
     }
   });
-});
+
+  $('#shoppingList').on('click', '.deleteBtn', function(e) {
+    $(this)
+      .parent()
+      .remove();
+  });
+};
+main();
